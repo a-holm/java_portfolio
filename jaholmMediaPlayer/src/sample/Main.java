de3075aeb8d3ca77a8d6dbb1db93d44dcfd20054
a-holm <jaholm.consulting@gmail.com>
2017-08-21 19:57:32 +0200

    Add functionality to open new media files in  the menu bar

--- jaholmMediaPlayer/src/sample/Main.java
+++ jaholmMediaPlayer/src/sample/Main.java
@@ -1,27 +1,60 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-import java.awt.*;
+import java.io.File;
+import java.net.MalformedURLException;
 
 public class Main extends Application {
+    FileChooser fileChooser;
+    //Change this to a local file
+    Player player = new Player("file:///J:/sample.mp4");
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Player player = new Player("file:///J:/testvideo512kb.mp4");
-        Scene scene = new Scene(player, 320,270, Color.DARKCYAN);
+    public void start(final Stage primaryStage) throws Exception{
+        MenuItem open = new MenuItem("Open");
+        Menu file = new Menu("File");
+        MenuBar menu = new MenuBar();
+
+        file.getItems().add(open);
+        menu.getMenus().add(file);
+
+        fileChooser = new FileChooser();
+
+        open.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                System.out.println("test1");
+                player.player.pause();
+
+                File file = fileChooser.showOpenDialog(primaryStage);
+
+                if (file != null){
+                    try {
+                        player = new Player(file.toURI().toURL().toExternalForm());
+                        Scene scene = new Scene(player, 560,380, Color.DARKCYAN);
+                        primaryStage.setScene(scene);
+                    } catch (MalformedURLException e1){
+
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        player.setTop(menu);
+        Scene scene = new Scene(player, 560,380, Color.DARKCYAN);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Jaholm Media Player");
         primaryStage.show();
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//
-//
-//
+
     }
 
 
